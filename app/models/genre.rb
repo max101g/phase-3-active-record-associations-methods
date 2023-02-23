7917c46381f99b24e194fdf3333d11fa183c6b75
max101g <maxgithinji101@gmail.com>
2023-02-24 01:29:05 +0300

    Ruby commit

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    s = self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    a = self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    all =  self.artists.pluck(:name)
   end
 end
